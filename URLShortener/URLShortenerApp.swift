39d4bfb74e7fe96aef296a31a418f2cf2a003623
Daryl <daryl.9@gmail.com>
2021-08-14 08:19:52 +0800

    Initial Commit

---
+++ URLShortener/URLShortenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortenerApp.swift
+//  URLShortener
+//
+//  Created by admin on 14/8/21.
+//
+
+import SwiftUI
+
+@main
+struct URLShortenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
