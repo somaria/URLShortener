357d129ee47f170f76cda3685992b8b0655044f9
Daryl <daryl.9@gmail.com>
2021-08-14 15:32:29 +0800

    mm

--- URLShortener/ContentView.swift
+++ URLShortener/ContentView.swift
@@ -8,10 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @StateObject var viewModel = ViewModel()
+  
+  @State var text: String = ""
+  
+  @ViewBuilder
+  func header() -> some View {
+    VStack {
+      Text("Enter URL to be shortened")
+        .bold()
+        .font(.system(size: 26))
+        .foregroundColor(.white)
+        .padding()
+      
+      TextField("URL...", text: $text)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(8)
+        .padding()
+      
+      Button {
+        viewModel.submit(urlString: text)
+//        print(viewModel.models[0].long)
+      } label: {
+        Text("Submit URL")
+          .bold()
+          .foregroundColor(.white)
+          .frame(width: 240, height: 50)
+          .background(Color.pink)
+          .cornerRadius(8)
+          .padding()
+      }
+
     }
+    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
+    .background(Color.blue)
+  }
+  
+    var body: some View {
+      NavigationView {
+        ScrollView(.vertical) {
+          header()
+
+          ForEach(viewModel.models, id:\.self) { model in
+            Text("test")
+            Text(model.long)
+            Text(model.short)
+          }
+          
+        }
+        .navigationTitle("URL Shortener")
+      }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +69,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ URLShortener/ViewModel.swift
@@ -0,0 +1,82 @@
+//
+//  ViewModel.swift
+//  ViewModel
+//
+//  Created by admin on 14/8/21.
+//
+
+import SwiftUI
+
+class ViewModel: ObservableObject {
+  
+  @Published var models = [Model]()
+  
+  func submit(urlString: String) {
+    print("submitting")
+    
+    guard URL(string: urlString) != nil else { return }
+    
+    print(urlString)
+    
+    guard let apiUrl = URL(string: "https://api.1pt.co/addURL?long=" + urlString.lowercased()) else { return }
+    
+    print(apiUrl)
+    
+    let task = URLSession.shared.dataTask(with: apiUrl) { data, _, error in
+      
+      guard let data = data, error == nil else { return }
+      
+      do {
+        let result = try JSONDecoder().decode(APIResponse.self, from: data)
+        
+        let long = result.long
+        let short = result.short
+        let status = result.status
+        
+        print(long)
+        print(short)
+        print(status)
+        
+        DispatchQueue.main.async {
+          self.models.append(.init(long: long, short: short))
+          print(self.models[0].short)
+        }
+        
+        print("89fd")
+        
+        
+        
+      } catch {
+        print(error)
+      }
+    }
+    
+    task.resume()
+    
+  }
+}
+
+struct Model: Hashable {
+  let long: String
+  let short: String
+}
+
+struct APIResponse: Codable {
+  let status: Int
+  let message: String
+  let short: String
+  let long: String
+}
+
+
+
+//https://api.1pt.co/addURL?long=https://apple.com
+/*
+ {
+ status: 201,
+ message: "Added!",
+ short: "btkwp",
+ long: "https://apple.com"
+ }
+ */
+
